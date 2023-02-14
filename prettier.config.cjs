e3f7602473c17fd7b098c9275fa8f3efdad3201d
QriLa <rea9lizer@gmail.com>
2023-02-15 01:41:17 +0900

    🚨 Format existing files with new rules

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,7 +1,7 @@
 /** @type {import("prettier").Config} */
 module.exports = {
-  plugins: [require.resolve("prettier-plugin-tailwindcss")],
-  tabWidth: 3,
-  useTabs: true,
-  semi: false,
-};
+	plugins: [require.resolve("prettier-plugin-tailwindcss")],
+	tabWidth: 3,
+	useTabs: true,
+	semi: false,
+}
