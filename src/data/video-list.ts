0021995bdc770229b6c48cf82e5986f14bbaf401
QriLa <rea9lizer@gmail.com>
2023-02-16 15:48:31 +0900

    🗃️ Add static data of video list

---
+++ src/data/video-list.ts
@@ -0,0 +1,136 @@
+type Version = {
+	name: string
+	isSelectable: boolean
+	selectedByDefault: boolean
+	videoUrl?: string
+}
+
+const versionList: Array<Version> = [
+	{
+		name: "S2 합작버전",
+		isSelectable: true,
+		selectedByDefault: true,
+	},
+	{
+		name: "S2 합작 노래방버젼",
+		isSelectable: true,
+		selectedByDefault: true,
+	},
+	{
+		name: "S2 합작 쇼츠버젼",
+		isSelectable: false,
+		selectedByDefault: false,
+	},
+	{
+		name: "S2 합작 바쁜 사람들을 위한버젼",
+		isSelectable: false,
+		selectedByDefault: false,
+	},
+	{
+		name: "S2 합작 감동버젼",
+		isSelectable: true,
+		selectedByDefault: false,
+	},
+	{
+		name: "S2 합작 휴대용버젼",
+		isSelectable: true,
+		selectedByDefault: false,
+	},
+	{
+		name: "S2 합작 황정민이 지구를 지키는버젼",
+		isSelectable: false,
+		selectedByDefault: false,
+	},
+	{
+		name: "S2 합작 그림판버젼",
+		isSelectable: false,
+		selectedByDefault: false,
+	},
+	{
+		name: "S2 합작 일본어자막버젼",
+		isSelectable: true,
+		selectedByDefault: false,
+	},
+	{
+		name: "S2 합작 시간여행버젼",
+		isSelectable: false,
+		selectedByDefault: false,
+	},
+	{
+		name: "S2 합작 '다'가 나올 때마다 음이 높아지는버젼",
+		isSelectable: true,
+		selectedByDefault: false,
+	},
+	{
+		name: "S2 합작 183버젼",
+		isSelectable: false,
+		selectedByDefault: false,
+	},
+	{
+		name: "S2 합작 1432버젼",
+		isSelectable: true,
+		selectedByDefault: false,
+	},
+	{
+		name: "S2 합작 디제잉버젼",
+		isSelectable: false,
+		selectedByDefault: false,
+	},
+	{
+		name: "S2 합작 잠수함버젼",
+		isSelectable: true,
+		selectedByDefault: false,
+	},
+	{
+		name: "S2 합작 소스가 겹치면 안되는버젼",
+		isSelectable: true,
+		selectedByDefault: true,
+	},
+	{
+		name: "S2 합작 칩튠버젼",
+		isSelectable: true,
+		selectedByDefault: false,
+	},
+	{
+		name: "S2 합작 사각버젼",
+		isSelectable: true,
+		selectedByDefault: false,
+	},
+	{
+		name: "S2 합작 영화관버젼",
+		isSelectable: true,
+		selectedByDefault: false,
+	},
+	{
+		name: "S2 합작 AI버젼",
+		isSelectable: true,
+		selectedByDefault: true,
+	},
+	{
+		name: "S2 합작 없는 게 없는 무한도전버젼",
+		isSelectable: true,
+		selectedByDefault: false,
+	},
+	{
+		name: "S2 합작 퀴즈버젼",
+		isSelectable: true,
+		selectedByDefault: false,
+	},
+	{
+		name: "S2 합작 otoMAD-synthesis버젼",
+		isSelectable: false,
+		selectedByDefault: false,
+	},
+	{
+		name: "S2 합작 리마스터버젼",
+		isSelectable: true,
+		selectedByDefault: false,
+	},
+	{
+		name: "S2 합작 매쉬업버젼",
+		isSelectable: true,
+		selectedByDefault: false,
+	},
+]
+
+export default versionList
