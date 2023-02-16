2889b50b1e3ae9a666f7004b2c2542dc7f2d5b6e
QriLa <rea9lizer@gmail.com>
2023-02-17 06:29:57 +0900

    🗃️ Improve data related namings

--- src/data/video-list.ts
+++ src/data/video-list.ts
@@ -1,161 +1,161 @@
-type Version = {
-	name: string
+export type VideoInfo = {
+	title: string
 	isSelectable: boolean
 	selectedByDefault: boolean
 	videoId: string
 }
 
-const versionList: Array<Version> = [
+const videoList: VideoInfo[] = [
 	{
-		name: "S2 합작버전",
+		title: "S2 합작버전",
 		isSelectable: true,
 		selectedByDefault: true,
 		videoId: "OJuHhpnoJ6s",
 	},
 	{
-		name: "S2 합작 노래방버젼",
+		title: "S2 합작 노래방버젼",
 		isSelectable: true,
 		selectedByDefault: true,
 		videoId: "fZas_ZUhCk4",
 	},
 	{
-		name: "S2 합작 쇼츠버젼",
+		title: "S2 합작 쇼츠버젼",
 		isSelectable: false,
 		selectedByDefault: false,
 		videoId: "xlF_VsTkc60",
 	},
 	{
-		name: "S2 합작 바쁜 사람들을 위한버젼",
+		title: "S2 합작 바쁜 사람들을 위한버젼",
 		isSelectable: false,
 		selectedByDefault: false,
 		videoId: "al_jfuVWYrI",
 	},
 	{
-		name: "S2 합작 감동버젼",
+		title: "S2 합작 감동버젼",
 		isSelectable: true,
 		selectedByDefault: false,
 		videoId: "QAMvCusAdxs",
 	},
 	{
-		name: "S2 합작 휴대용버젼",
+		title: "S2 합작 휴대용버젼",
 		isSelectable: true,
 		selectedByDefault: false,
 		videoId: "QfeV1RfX3rg",
 	},
 	{
-		name: "S2 합작 황정민이 지구를 지키는버젼",
+		title: "S2 합작 황정민이 지구를 지키는버젼",
 		isSelectable: false,
 		selectedByDefault: false,
 		videoId: "1M8kyR-XacE",
 	},
 	{
-		name: "S2 합작 그림판버젼",
+		title: "S2 합작 그림판버젼",
 		isSelectable: false,
 		selectedByDefault: false,
 		videoId: "oKh2hURr3I0",
 	},
 	{
-		name: "S2 합작 일본어자막버젼",
+		title: "S2 합작 일본어자막버젼",
 		isSelectable: true,
 		selectedByDefault: false,
 		videoId: "GZHdIbC1rQk",
 	},
 	{
-		name: "S2 합작 시간여행버젼",
+		title: "S2 합작 시간여행버젼",
 		isSelectable: false,
 		selectedByDefault: false,
 		videoId: "lJnjCJDOwWg",
 	},
 	{
-		name: "S2 합작 '다'가 나올 때마다 음이 높아지는버젼",
+		title: "S2 합작 '다'가 나올 때마다 음이 높아지는버젼",
 		isSelectable: true,
 		selectedByDefault: false,
 		videoId: "mgtktVnf0cQ",
 	},
 	{
-		name: "S2 합작 183버젼",
+		title: "S2 합작 183버젼",
 		isSelectable: false,
 		selectedByDefault: false,
 		videoId: "OKWLmKGkEEk",
 	},
 	{
-		name: "S2 합작 1432버젼",
+		title: "S2 합작 1432버젼",
 		isSelectable: true,
 		selectedByDefault: false,
 		videoId: "sZCUsKCELzc",
 	},
 	{
-		name: "S2 합작 디제잉버젼",
+		title: "S2 합작 디제잉버젼",
 		isSelectable: false,
 		selectedByDefault: false,
 		videoId: "xCRsYInpEqM",
 	},
 	{
-		name: "S2 합작 잠수함버젼",
+		title: "S2 합작 잠수함버젼",
 		isSelectable: true,
 		selectedByDefault: false,
 		videoId: "Tzw249nH2X0",
 	},
 	{
-		name: "S2 합작 소스가 겹치면 안되는버젼",
+		title: "S2 합작 소스가 겹치면 안되는버젼",
 		isSelectable: true,
 		selectedByDefault: true,
 		videoId: "R-GURSYntn8",
 	},
 	{
-		name: "S2 합작 칩튠버젼",
+		title: "S2 합작 칩튠버젼",
 		isSelectable: true,
 		selectedByDefault: false,
 		videoId: "c8vyHZmVyUI",
 	},
 	{
-		name: "S2 합작 사각버젼",
+		title: "S2 합작 사각버젼",
 		isSelectable: true,
 		selectedByDefault: false,
 		videoId: "-re2zXyC9ZA",
 	},
 	{
-		name: "S2 합작 영화관버젼",
+		title: "S2 합작 영화관버젼",
 		isSelectable: true,
 		selectedByDefault: false,
 		videoId: "PWR-P916Wxs",
 	},
 	{
-		name: "S2 합작 AI버젼",
+		title: "S2 합작 AI버젼",
 		isSelectable: true,
 		selectedByDefault: true,
 		videoId: "HbyHo5iblN4",
 	},
 	{
-		name: "S2 합작 없는 게 없는 무한도전버젼",
+		title: "S2 합작 없는 게 없는 무한도전버젼",
 		isSelectable: true,
 		selectedByDefault: false,
 		videoId: "ueofEV8iKA0",
 	},
 	{
-		name: "S2 합작 퀴즈버젼",
+		title: "S2 합작 퀴즈버젼",
 		isSelectable: true,
 		selectedByDefault: false,
 		videoId: "g6e0gV0ZruQ",
 	},
 	{
-		name: "S2 합작 otoMAD-synthesis버젼",
+		title: "S2 합작 otoMAD-synthesis버젼",
 		isSelectable: false,
 		selectedByDefault: false,
 		videoId: "oTPsDT-LlMk",
 	},
 	{
-		name: "S2 합작 리마스터버젼",
+		title: "S2 합작 리마스터버젼",
 		isSelectable: true,
 		selectedByDefault: false,
 		videoId: "4dLzBdwqNfA",
 	},
 	{
-		name: "S2 합작 매쉬업버젼",
+		title: "S2 합작 매쉬업버젼",
 		isSelectable: true,
 		selectedByDefault: false,
 		videoId: "QIJYmohOc9k",
 	},
 ]
 
-export default versionList
+export default videoList
